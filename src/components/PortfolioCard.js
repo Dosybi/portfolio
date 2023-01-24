2c94cf62c32fd22146e42f5cb0e3ee9488899bfd
Dosybi <dosybi@gmail.com>
2023-01-25 00:03:57 +0600

    update portfolio card

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -12,25 +12,21 @@ const PortfolioCard = ({
   isLast,
 }) => {
   return (
-    <div
-      className={`mb-4 box-border overflow-hidden pt-9 pb-10 ${
-        isLast ? 'border-b-0' : 'border-b-2 dark:border-slate-500'
-      } border-black`}
-    >
+    <div className={`mb-4 box-border overflow-hidden pt-9 pb-10`}>
       <div className="md:flex md:flex-row-reverse md:justify-between">
         {/* <Image className="mb-3 h-full w-1/3" src={image} alt={title} /> */}
         <div className="md:mr-8">
-          <div className="mb-2 text-xs uppercase tracking-widest text-red-600 dark:text-red-400">
-            {category}
-          </div>
           <h2 className="text-3xl font-bold uppercase md:mr-8 md:text-4xl lg:text-6xl">
             {title}
           </h2>
           <div className="mb-2 text-sm">{year}</div>
-          <h3 className="font-italic mb-4 text-lg text-gray-600 dark:text-slate-300">
+          <div className="mb-4 text-xs uppercase tracking-widest text-red-600 dark:text-red-400">
+            {category}
+          </div>
+          {/* <h3 className="font-italic mb-4 text-lg text-gray-600 dark:text-slate-300">
             {subtitle}
-          </h3>
-          <p className="mb-8">{text}</p>
+          </h3> */}
+          <p className="mb-4">{text}</p>
           <div className="self-end">
             <Button {...button} />
           </div>
