f10b2d1d6ff631bbeca6f530485d6c998654a9b9
Dosybi <dosybi@gmail.com>
2023-02-02 17:15:59 +0600

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = ({ name }) => {
   return (
     <footer className="mt-10 text-xs">
       <div>
-        © {name}, {new Date().getFullYear()} | Make love, not war ❤️
+        © {name}, {new Date().getFullYear()} | Make love, not war ❤️
       </div>
     </footer>
   )
